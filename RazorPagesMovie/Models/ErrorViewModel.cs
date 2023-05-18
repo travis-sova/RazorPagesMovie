a41723b68d885bfbec558263922a84c4af4a946d
travis-sova <124631581+travis-sova@users.noreply.github.com>
2023-05-18 21:57:02 +0300

    Add files via upload

---
+++ RazorPagesMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RazorPagesMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
